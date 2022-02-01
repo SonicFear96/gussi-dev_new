9789c61a9d7d5ed3832b2ce96f41e38f3c383059
Bogdan Rudakov <48532613+SonicFear96@users.noreply.github.com>
2022-02-01 18:08:32 +0300

    init: start project + gulp + html + css

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+const { src, dest, series, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const csso = require("gulp-csso");
+const htmlmin = require("gulp-htmlmin");
+const del = require("del");
+const concat = require("gulp-concat");
+const autoprefixer = require("gulp-autoprefixer");
+const sync = require("browser-sync").create();
+const ghPages = require("gh-pages");
+const path = require("path");
+const sourcemaps = require("gulp-sourcemaps");
+
+function deploy(cb) {
+  ghPages.publish(path.join(process.cwd(), "./dist"), cb);
+}
+
+function html() {
+  return src("src/**.html")
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      })
+    )
+    .pipe(dest("dist"));
+}
+
+function scss() {
+  return src("src/scss/**scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 2 versions"],
+      })
+    )
+    .pipe(csso())
+    .pipe(concat("style.css"))
+    .pipe(sourcemaps.write())
+    .pipe(dest("dist"));
+}
+
+function clear() {
+  return del("dist");
+}
+
+function image() {
+  return src("src/img/**/*.{png,jpg,svg}").pipe(dest("dist/img"));
+}
+
+function scripts() {
+  return src("src/**.js")
+    .pipe(sourcemaps.init())
+    .pipe(concat("index.js"))
+    .pipe(sourcemaps.write())
+    .pipe(dest("dist"));
+}
+
+function copy() {
+  return src("src/fonts/*.{woff2,woff}").pipe(dest("dist/fonts"));
+}
+
+function serve() {
+  sync.init({
+    server: "./dist",
+  });
+
+  watch("src/**.html", series(html)).on("change", sync.reload);
+  watch("src/scss/**/*.scss", series(scss)).on("change", sync.reload);
+}
+
+exports.build = series(clear, scss, html, scripts, image, copy);
+exports.start = series(clear, scss, html, scripts, image, copy, serve);
+exports.deploy = deploy;
