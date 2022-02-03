035690ef2deaf449f990e06c3e62d3b6c4e8f749
Bogdan Rudakov <48532613+SonicFear96@users.noreply.github.com>
2022-02-03 20:14:18 +0300

    fix: slider js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,3 @@
-// let slideIndex = 1;
-// showSlides(slideIndex);
-// function rightSlide() {
-//   showSlides((slideIndex += 1));
-// }
-// function leftSlide() {
-//   showSlides((slideIndex -= 1));
-// }
-// function currentSlide(n) {
-//   showSlides((slideIndex = n));
-// }
-// function showSlides(n) {
-//   let slider = document.getElementsByClassName("slider");
-//   console.log(slider);
-//   if (n > slider.length) {
-//     slideIndex = 1;
-//   }
-//   if (n < 1) {
-//     slideIndex = slider.length;
-//   }
-//   for (let i = 0; i < slider.length; i++) {
-//     slider[i].style.display = "none";
-//   }
-
-//   slider[slideIndex - 1].style.display = "block";
-// }
-
 /*Quantity */
 
 const LeftQuantity = document.querySelector(".quantity__left");
@@ -50,6 +23,9 @@ const ButtonSliderNext = document.querySelector(".slider__next");
 const ButtonSliderPrev = document.querySelector(".slider__prev");
 const CounterFirst = document.querySelector(".counter-first");
 const CounterLast = document.querySelector(".counter-last");
+const SliderTitle = document.querySelector(".slider__title");
+
+const arrSliderTitle = ["Light coat", "Sandy longsleeve", "Lime jacket"];
 
 let i = 1;
 
@@ -57,8 +33,10 @@ const NextSlider = () => {
   if (i === 3) {
     ButtonSliderNext.disabled = true;
   } else i++;
+  SliderTitle.textContent = arrSliderTitle[i - 1];
   ButtonSliderNext.disabled = false;
   CounterFirst.textContent = `0${i}`;
+
   Slider.style.backgroundImage = `url('./img/gussi_slider_${i}.png')`;
 };
 
@@ -66,6 +44,7 @@ const PrevSlider = () => {
   if (i === 1) {
     ButtonSliderPrev.disabled = true;
   } else i--;
+  SliderTitle.textContent = arrSliderTitle[i - 1];
   ButtonSliderPrev.disabled = false;
   CounterFirst.textContent = `0${i}`;
   Slider.style.backgroundImage = `url('./img/gussi_slider_${i}.png')`;
